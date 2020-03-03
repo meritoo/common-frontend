10c77c92c2015f8267d64abca6a4b729446c7ef3
Meritoo <github@meritoo.pl>
2020-03-03 12:57:16 +0100

    Add main JavaScript file with all utilities imported

---
+++ src/js/utilities/index.js
@@ -0,0 +1,13 @@
+const arrays = require('./arrays');
+const miscellaneous = require('./miscellaneous');
+const numbers = require('./numbers');
+const objects = require('./objects');
+const string = require('./string');
+
+module.exports = {
+    arrays: arrays,
+    miscellaneous: miscellaneous,
+    numbers: numbers,
+    objects: objects,
+    string: string,
+};
