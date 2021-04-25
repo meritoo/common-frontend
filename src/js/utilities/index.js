e976302f7e9d51efe16daf108b08802f3a26d6d4
Meritoo <github@meritoo.pl>
2021-04-25 09:57:42 +0200

    Reexport regex utilities

--- src/js/utilities/index.js
+++ src/js/utilities/index.js
@@ -1,13 +1,15 @@
-import * as Arrays from './arrays';
-import * as Miscellaneous from './miscellaneous';
-import * as Numbers from './numbers';
-import * as Objects from './objects';
-import * as Strings from './strings';
+import * as Arrays from './arrays'
+import * as Miscellaneous from './miscellaneous'
+import * as Numbers from './numbers'
+import * as Objects from './objects'
+import * as Regex from './regex'
+import * as Strings from './strings'
 
 export {
     Arrays,
     Miscellaneous,
     Numbers,
     Objects,
+    Regex,
     Strings,
 };
